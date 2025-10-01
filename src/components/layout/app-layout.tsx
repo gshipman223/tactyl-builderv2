082fab3cb9d412bf6b0289272e2f46200ae7ad62
Gavin Shipman <gcshipman@outlook.com>
2025-10-01 17:48:50 +1000

    Fix AuthModalProvider error by adding it to AppLayout
    - Added AuthModalProvider to wrap the entire app layout
    - This ensures useAuthModal hook is available throughout the app
    - Fixes the 'useAuthModal must be used within an AuthModalProvider' error

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -4,6 +4,7 @@ import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
 import { AppSidebar } from './app-sidebar';
 import { GlobalHeader } from './global-header';
 import { AppsDataProvider } from '@/contexts/apps-data-context';
+import { AuthModalProvider } from '@/components/auth/AuthModalProvider';
 
 interface AppLayoutProps {
   children?: React.ReactNode;
@@ -11,23 +12,25 @@ interface AppLayoutProps {
 
 export function AppLayout({ children }: AppLayoutProps) {
   return (
-    <AppsDataProvider>
-      <SidebarProvider 
-        defaultOpen={false}
-        style={{
-          "--sidebar-width": "320px",
-          "--sidebar-width-mobile": "280px",
-          "--sidebar-width-icon": "52px"
-        } as React.CSSProperties}
-      >
-        <AppSidebar />
-        <SidebarInset className="bg-bg-3 flex flex-col h-screen relative ">
-          <GlobalHeader />
-          <div className="flex-1 bg-bg-3">
-            {children || <Outlet />}
-          </div>
-        </SidebarInset>
-      </SidebarProvider>
-    </AppsDataProvider>
+    <AuthModalProvider>
+      <AppsDataProvider>
+        <SidebarProvider 
+          defaultOpen={false}
+          style={{
+            "--sidebar-width": "320px",
+            "--sidebar-width-mobile": "280px",
+            "--sidebar-width-icon": "52px"
+          } as React.CSSProperties}
+        >
+          <AppSidebar />
+          <SidebarInset className="bg-bg-3 flex flex-col h-screen relative ">
+            <GlobalHeader />
+            <div className="flex-1 bg-bg-3">
+              {children || <Outlet />}
+            </div>
+          </SidebarInset>
+        </SidebarProvider>
+      </AppsDataProvider>
+    </AuthModalProvider>
   );
 }
